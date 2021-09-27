b2d86bd9d529d798a8275e5bb6cef7075869e180
kostas-s <65230255+kostas-s@users.noreply.github.com>
2021-09-27 21:41:28 +0300

    url path fix for production

--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -16,7 +16,7 @@ const PortfolioContainer = () => {
     const token = document.querySelector("[name=csrf-token]").content;
     axios.defaults.headers.common["X-CSRF-TOKEN"] = token;
     axios
-      .post("http://localhost:3000/search", {
+      .post("/search", {
         search: evt.target.value,
       })
       .then((data) => {
